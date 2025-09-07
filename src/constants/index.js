202d9d14f0e891afce0e1a253a44343608a0bfd9
thenoobchad <noobchaddy@gmail.com>
2025-09-07 05:59:56 +0100

    installed tailwindcss & gsap

---
+++ src/constants/index.js
@@ -0,0 +1,36 @@
+export const servicesData = [
+    {
+        title: "Web Development",
+        description: "Building responsive and dynamic websites using modern technologies.",
+        items: [
+            {
+                title: "Backend Engineering",
+                description: "Creating robust server-side applications and APIs."
+            },
+            {
+                title: "Frontend Engineering",
+                description: "(React, Vue Typescript, Interractive UI/UX)",
+            },
+            {
+                title: "Fullstack Development",
+                description: "Combining both frontend and backend skills to build complete web applications."
+            },
+        ]
+    }
+]
+
+export const socials = [
+    {
+        name: "GitHub",
+        href:"www.github.com/chidielueme"
+    },
+    {
+        name: "LinkedIn",
+        href:"www.linkedin.com/in/chidielueme"
+    },
+    {
+        name: "Twitter",
+        href:"www.twitter.com/chidielueme"
+    },
+    
+]
